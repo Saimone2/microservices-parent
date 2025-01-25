146890869ef79e4b38e2916bd3e320be2c34254a
Saimone2 <hav1802200474@gmail.com>
2025-01-25 10:18:43 +0200

    the foundation for microservice architecture has been created

---
+++ gateway-service/src/main/java/com/sa1mone/gateway_service/ApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.sa1mone.gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
